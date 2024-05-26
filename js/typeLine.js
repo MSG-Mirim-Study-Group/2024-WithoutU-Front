474fce665c65a5e1b4d54b8de70e27d516cd4230
subin1848 <s2315@e-mirim.hs.kr>
2024-05-26 20:28:08 +0900

    ✨[feat] typeLine 수정 2

---
+++ js/typeLine.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.saying').classList.add("fade-in");
+    }, 500);
+
+    // 3초 후에 리디렉션
+    setTimeout(function() {
+        window.location.href = "result.html";
+    }, 3000);
+});
+
